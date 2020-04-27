b63e13ccc56ab4d876d2611d7a192b614415b49c
aotuotage <826931037@qq.com>
2020-04-27 10:22:24 +0800

    125151

---
+++ build/precache-manifest.f7867b37914748446cb5b374ea1d2748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7a6aac9eb5296ddbf95ffaf732d7d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aa43d53551fd327bdf7",
+    "url": "/static/css/2.30dffb32.chunk.css"
+  },
+  {
+    "revision": "d1a3336fea53f574f264",
+    "url": "/static/css/main.78839bbf.chunk.css"
+  },
+  {
+    "revision": "4aa43d53551fd327bdf7",
+    "url": "/static/js/2.26453216.chunk.js"
+  },
+  {
+    "revision": "d1a3336fea53f574f264",
+    "url": "/static/js/main.c8af3a7e.chunk.js"
+  },
+  {
+    "revision": "d4e9e18d55af53042b49",
+    "url": "/static/js/runtime-main.81f5f437.js"
+  }
+]);
